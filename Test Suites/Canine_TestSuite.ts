eab39777c156706c3c1132f64f995f31e26521f1
Radhakrishnan <radhakrishnang2@nih.gov>
2020-05-12 08:16:04 -0700

    removed profile

--- Test Suites/Canine_TestSuite.ts
+++ Test Suites/Canine_TestSuite.ts
@@ -13,19 +13,19 @@
    <testCaseLink>
       <guid>426999de-2932-4e9f-9fcb-9db3d37928b8</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/Study/TC01_Canine_Filter_Study-COTB</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>c6f035aa-0e4e-4f58-aa00-629ba873e94e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/Study/TC03_Canine_Filter_Study-ALL</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>ab5d1246-ca40-49b3-94c0-b94f1e77bdd8</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyType/TC01_Canine_Filter_StudyType-ClinicalTrial</testCaseId>
    </testCaseLink>
    <testCaseLink>
