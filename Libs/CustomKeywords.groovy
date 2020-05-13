de1afa2ba9d64bc41415b6934bb3dc87875245c3
Radhakrishnan <radhakrishnang2@nih.gov>
2020-05-12 17:12:44 -0700

    removed video recording from reports

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,149 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+import org.openqa.selenium.WebDriver
+
+import org.openqa.selenium.WebElement
+
+
+def static "ctdc.utilities.ConnectDB.main"() {
+    (new ctdc.utilities.ConnectDB()).main()
+}
+
+def static "ctdc.utilities.runtestcaseforKatalon.RunKatalon"(
+    	String input_file	) {
+    (new ctdc.utilities.runtestcaseforKatalon()).RunKatalon(
+        	input_file)
+}
+
+def static "ctdc.utilities.runtestcaseforKatalon.ReadCasesTableKatalon"(
+    	String tbl1	
+     , 	String hdr1	
+     , 	String nxtb1	) {
+    (new ctdc.utilities.runtestcaseforKatalon()).ReadCasesTableKatalon(
+        	tbl1
+         , 	hdr1
+         , 	nxtb1)
+}
+
+def static "ctdc.utilities.runtestcaseforKatalon.givexpath"(
+    	String objname	) {
+    (new ctdc.utilities.runtestcaseforKatalon()).givexpath(
+        	objname)
+}
+
+def static "ctdc.utilities.runtestcaseforKatalon.compareLists"() {
+    (new ctdc.utilities.runtestcaseforKatalon()).compareLists()
+}
+
+def static "ctdc.utilities.runtestcaseforKatalon.browserDriver"(
+    	String browserName	) {
+    (new ctdc.utilities.runtestcaseforKatalon()).browserDriver(
+        	browserName)
+}
+
+def static "ctdc.utilities.runtestcaseforKatalon.Select_case_checkbox"(
+    	String caseID	
+     , 	String count	) {
+    (new ctdc.utilities.runtestcaseforKatalon()).Select_case_checkbox(
+        	caseID
+         , 	count)
+}
+
+def static "ctdc.utilities.sandbox_g.createWebDataCSV"() {
+    (new ctdc.utilities.sandbox_g()).createWebDataCSV()
+}
+
+def static "ctdc.utilities.sandbox_g.comparison"() {
+    (new ctdc.utilities.sandbox_g()).comparison()
+}
+
+def static "ctdc.utilities.sandbox_g.readingCSVFile"() {
+    (new ctdc.utilities.sandbox_g()).readingCSVFile()
+}
+
+def static "ctdc.utilities.ReadExcel.Test"(
+    	String filename	) {
+    (new ctdc.utilities.ReadExcel()).Test(
+        	filename)
+}
+
+def static "ctdc.utilities.ReadExcel.readExceltoWeblist"(
+    	String filename	) {
+    (new ctdc.utilities.ReadExcel()).readExceltoWeblist(
+        	filename)
+}
+
+def static "ctdc.utilities.ReadExcel.Neo4j"() {
+    (new ctdc.utilities.ReadExcel()).Neo4j()
+}
+
+def static "ctdc.utilities.ReadExcel.initialLoad"() {
+    (new ctdc.utilities.ReadExcel()).initialLoad()
+}
+
+def static "ctdc.utilities.ReadExcel.PrintG"() {
+    (new ctdc.utilities.ReadExcel()).PrintG()
+}
+
+def static "ctdc.utilities.ReadExcel.ExcelToArray"(
+    	String filename	) {
+    (new ctdc.utilities.ReadExcel()).ExcelToArray(
+        	filename)
+}
+
+def static "ctdc.utilities.RunTestcase.Run"(
+    	String InputExcelname	
+     , 	String pwd_file	) {
+    (new ctdc.utilities.RunTestcase()).Run(
+        	InputExcelname
+         , 	pwd_file)
+}
+
+def static "ctdc.utilities.RunTestcase.ReadCasesTable"(
+    	WebDriver driver	) {
+    (new ctdc.utilities.RunTestcase()).ReadCasesTable(
+        	driver)
+}
+
+def static "ctdc.utilities.RunTestcase.compareLists"() {
+    (new ctdc.utilities.RunTestcase()).compareLists()
+}
+
+def static "ctdc.utilities.RunTestcase.browserDriver"(
+    	String browserName	) {
+    (new ctdc.utilities.RunTestcase()).browserDriver(
+        	browserName)
+}
+
+def static "ctdc.utilities.ExtraFunctions.compareLists_1D"() {
+    (new ctdc.utilities.ExtraFunctions()).compareLists_1D()
+}
+
+def static "ctdc.utilities.sandbox.CaseData"() {
+    (new ctdc.utilities.sandbox()).CaseData()
+}
+
+def static "ctdc.utilities.sandbox.getall"() {
+    (new ctdc.utilities.sandbox()).getall()
+}
+
+def static "ctdc.utilities.sandbox.getElementID"(
+    	WebElement Tab	
+     , 	String caseid	) {
+    (new ctdc.utilities.sandbox()).getElementID(
+        	Tab
+         , 	caseid)
+}
+
+def static "ctdc.utilities.sandbox.clicking"() {
+    (new ctdc.utilities.sandbox()).clicking()
+}
+
+def static "ctdc.utilities.sandbox.tablesize"() {
+    (new ctdc.utilities.sandbox()).tablesize()
+}
