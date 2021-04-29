f86e49223a507dec4910b7cd8ab3541093019a79
Radhakrishnan <radhakrishnang2@nih.gov>
2021-04-29 10:58:47 -0700

    5 tc for bento

--- Test Suites/Bento_TestSuite.ts
+++ Test Suites/Bento_TestSuite.ts
@@ -44,37 +44,37 @@
    <testCaseLink>
       <guid>2692292b-9325-487c-9d73-2e7cefbe02cd</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Bento_TestCases/MultipleFilters/TC06_Bento_MultiFilter_Arm-Diagnosis-TumorSize-PRStatus-EndocrineTher</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>96ff8e4f-96db-494d-86f7-54bc022398a4</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Bento_TestCases/MultipleFilters/TC07_Bento_MultiFilter_Arm-Diagnosis-TumorSize-PRStatus-MenoStatus</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>bf84d575-58a2-4772-b1f3-0893b3a35659</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Bento_TestCases/MultipleFilters/TC08_Bento_MultiFilter_Diagnosis-TumorSize-PRStatus-EndocrineTher-TissueType</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>956cfb59-8b57-4191-8f75-d6c4d53940e1</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Bento_TestCases/MultipleFilters/TC09_Bento_MultiFilter_Diagnosis-TumorSize-PRStatus-EndocrineTher-TissueComp</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>371e917e-8946-4caa-b105-dab5b41fce48</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Bento_TestCases/MultipleFilters/TC10_Bento_MultiFilter_Diagnosis-TumorSize-ERStatus-PRStatus-EndocrineTher-FileAssoc</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>00d7474c-3c90-44b5-b7b3-f2aedc4f0d25</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
+      <isRun>false</isRun>
       <testCaseId>Test Cases/Bento_TestCases/MultipleFilters/TC11_Bento_MultiFilter_Diagnosis-TumorSize-PRStatus-EndocrineTher-FileType</testCaseId>
    </testCaseLink>
    <testCaseLink>
