56c911f5bc47ea827a3779f3753a857ea138f11a
Radhakrishnan <radhakrishnang2@nih.gov>
2021-05-14 11:15:32 -0700

    Bento single test scripts completed - trialrun ts added for jenkins test

---
+++ Test Suites/TrialRun.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TrialRun</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>860bcb87-6378-46de-ada7-a5dfd65f912f</testSuiteGuid>
+   <testCaseLink>
+      <guid>292ff85d-4819-41b5-9d7d-40ffdd0ac5f8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bento_TestCases/Arm/TC01_Bento_Filter_Arm-A</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a0c30bcf-d671-4ff4-be91-4083a75ac9fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bento_TestCases/Arm/TC02_Bento_Filter_Arm-B</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
