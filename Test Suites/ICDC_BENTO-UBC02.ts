37a6484100cc1ab9312429e4140bb4e6406444fc
Radhakrishnan <radhakrishnang2@nih.gov>
2021-05-19 08:30:20 -0700

    renamed test suite to ubc2

--- Test Suites/ICDC_BENTO-UBC02.ts
+++ Test Suites/ICDC_BENTO-UBC02.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TrialRun</name>
+   <name>ICDC_BENTO-UBC02</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
