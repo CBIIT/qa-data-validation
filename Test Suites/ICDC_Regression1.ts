bc08deb03b1e4d1f92f3d707130e2985f5e2f580
Radhakrishnan <radhakrishnang2@nih.gov>
2021-06-15 08:19:13 -0700

    separate regre suites for icdc

--- Test Suites/ICDC_Regression1.ts
+++ Test Suites/ICDC_Regression1.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>ICDC_BENTO-UBC01</name>
+   <name>ICDC_Regression1</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
--- Test Suites/ICDC_Regression2.ts
+++ Test Suites/ICDC_Regression2.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>ICDC_BENTO-UBC02</name>
+   <name>ICDC_Regression2</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
