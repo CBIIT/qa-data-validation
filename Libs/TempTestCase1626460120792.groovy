bc93106c32c4e937a15ed0d90bad93c82efe80b4
laxmi lolla <lollal@nih.gov>
2021-07-16 17:11:13 -0400

    changed the xlfile akita 01 and created regression suite laxmi_regression1

---
+++ Libs/TempTestCase1626460120792.groovy
@@ -0,0 +1,26 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+import com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner
+import com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\lollal\\AppData\\Local\\Temp\\1\\Katalon\\Test Cases\\Canine_TestCases\\StudyCombinations\\Laxmi_Regression01\\TC32_Canine_StudyUBC-Breed_RespToTreatmt_Sex_NeutStatus\\20210716_142840\\execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/Canine_TestCases/StudyCombinations/Laxmi_Regression01/TC32_Canine_StudyUBC-Breed_RespToTreatmt_Sex_NeutStatus', new TestCaseBinding('Test Cases/Canine_TestCases/StudyCombinations/Laxmi_Regression01/TC32_Canine_StudyUBC-Breed_RespToTreatmt_Sex_NeutStatus',[:]), FailureHandling.STOP_ON_FAILURE , true, true)
+    
