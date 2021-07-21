acbad1d73d0f46bdaf69bd76da8f4a323fc9be10
radhakrishnang2 <radhakrishnang2@NCI-02150368-L.nci.nih.gov>
2021-07-21 09:20:26 -0700

    timing fix in keyword, updated ubc01 tc1,2

---
+++ Object Repository/Canine/Filter/PrimDiseaseSite/BoneAppendicular_Chkbx.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>BoneAppendicular_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>b7b1f0bb-b8de-4ef0-b346-210b38efed10</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id='checkbox_Primary Disease Site_Bone (Appendicular)']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id='checkbox_Primary Disease Site_Bone (Appendicular)']</value>
+   </webElementProperties>
+</WebElementEntity>
