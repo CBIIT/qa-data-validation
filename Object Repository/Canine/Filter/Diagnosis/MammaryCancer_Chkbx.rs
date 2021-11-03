37d6d996019e403c739b9902d186c7c8f4196d28
Radhakrishnan <radhakrishnang2@nih.gov>
2021-11-03 08:48:31 -0700

    mgt01 1 script

---
+++ Object Repository/Canine/Filter/Diagnosis/MammaryCancer_Chkbx.rs
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>MammaryCancer_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>c643b6da-b160-4eae-95e5-93b8548ba301</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id='checkbox_Diagnosis_Mammary Cancer']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id='checkbox_Diagnosis_Mammary Cancer']</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Canine/Filter/PrimDiseaseSite/MammaryGland_Chkbx.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>MammaryGland_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>a782a91e-d942-4c58-bd02-4244a70647ac</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id='checkbox_Primary Disease Site_Mammary Gland']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id='checkbox_Primary Disease Site_Mammary Gland']</value>
+   </webElementProperties>
+</WebElementEntity>
