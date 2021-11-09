f3847a2b53904b81eb32cf175af57c5c6ecd200c
sohil-nih <sohil.nih@gmail.com>
2021-11-09 11:06:21 -0500

    Automated TC02_Canine_StudyCOTC022

--- Object Repository/Canine/Filter/Breed/Anatolin_Shphrd_Dog_Chkbx.rs
+++ Object Repository/Canine/Filter/Breed/Anatolin_Shphrd_Dog_Chkbx.rs
@@ -1,13 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
-   <description></description>
-   <name>BoneApendicular_Chkbx</name>
+   <description>This is 'Akita' checkbox inside Breed filter</description>
+   <name>Anatolin_Shphrd_Dog_Chkbx</name>
    <tag></tag>
-   <elementGuidId>4ca97251-36b4-4742-b655-02c93753b89e</elementGuidId>
+   <elementGuidId>02d6e381-ba2a-4912-a43b-e1a70d09f605</elementGuidId>
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value> //*[@id='checkbox_Primary Disease Site_Bone (Appendicular)']</value>
+         <value>//*[@id='checkbox_Breed_Anatolian Shepherd Dog']</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -17,6 +17,6 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value> //*[@id='checkbox_Primary Disease Site_Bone (Appendicular)']</value>
+      <value>//*[@id='checkbox_Breed_Anatolian Shepherd Dog']</value>
    </webElementProperties>
 </WebElementEntity>
