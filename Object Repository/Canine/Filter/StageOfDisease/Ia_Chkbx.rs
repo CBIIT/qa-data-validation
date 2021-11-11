9350f184f8fb9b46ebb28352d85ddf2133913bdc
Radhakrishnan <radhakrishnang2@nih.gov>
2021-11-10 16:02:46 -0800

    6 new scripts mgt

---
+++ Object Repository/Canine/Filter/StageOfDisease/Ia_Chkbx.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Ia_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>5409017d-6b90-46c5-ac34-f9b2cabeb084</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id='checkbox_Stage of Disease_Ia']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id='checkbox_Stage of Disease_Ia']</value>
+   </webElementProperties>
+</WebElementEntity>
