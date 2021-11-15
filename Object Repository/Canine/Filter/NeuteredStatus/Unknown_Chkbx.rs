ea811aa41b62da41ce0232327cc2d2554ceaa4c1
Radhakrishnan <radhakrishnang2@nih.gov>
2021-11-15 11:45:56 -0800

    mgt 11 scripts

--- Object Repository/Canine/Filter/NeuteredStatus/Unknown_Chkbx.rs
+++ Object Repository/Canine/Filter/NeuteredStatus/Unknown_Chkbx.rs
@@ -6,21 +6,21 @@
    <elementGuidId>f4135c25-164b-4101-b837-82fc93e7fda6</elementGuidId>
    <selectorCollection>
       <entry>
+         <key>BASIC</key>
+         <value>//*[@id='checkbox_Neutered Status_Unknown']</value>
+      </entry>
+      <entry>
          <key>XPATH</key>
          <value>(//*[contains(text(),'Unknown')])[3]</value>
       </entry>
-      <entry>
-         <key>BASIC</key>
-         <value>(//*[contains(text(),'Unknown')])[3]</value>
-      </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
    <webElementProperties>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>(//*[contains(text(),'Unknown')])[3]</value>
+      <value>//*[@id='checkbox_Neutered Status_Unknown']</value>
    </webElementProperties>
 </WebElementEntity>
