e46ff79ddf91b32ece308db385546a656b4a2649
sohil-nih <65736146+sohil-nih@users.noreply.github.com>
2022-04-13 09:26:22 -0400

    Added CDS TC01 Accesses and Identified locators

---
+++ Object Repository/CDS/Data_page/Filter/ExperimentalStrategies/FaberEstSuaeQuisqueFortunae-Chkbx.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>FaberEstSuaeQuisqueFortunae-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>fd325ddc-238d-4400-a38a-a738a467b1ca</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Experimental Strategies_faber_est_suae_quisque_fortunae&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Experimental Strategies_faber_est_suae_quisque_fortunae&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
