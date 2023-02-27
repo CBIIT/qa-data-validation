4b205cb99baece1de9ac9d905495a04938d8f3f6
kallakuriv2 <kallakuriv2@host.docker.internal>
2023-02-27 14:07:48 -0500

    HTAn

---
+++ Object Repository/CDS/Filter/Study/CDS-Study-Molecular Char Init-Chkbx.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>CDS-Study-Molecular Char Init-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>2ee8ebbe-8ba8-46ba-bbb0-74baef09e871</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_Study_Molecular Characterization Initiative&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_Study_Molecular Characterization Initiative&quot;]</value>
+      <webElementGuid>b25e4bd5-38b6-43eb-80ec-42af796a2e9a</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
--- Scripts/CDS_TestCases/Study/TC03_CDS_Filter_Study-Molecular Char Init/Script1677522388666.groovy
+++ Scripts/CDS_TestCases/Study/TC03_CDS_Filter_Study-Molecular Char Init/Script1677522388666.groovy
@@ -41,7 +41,10 @@ import java.nio.file.Paths as Paths
  */
 WebUI.closeBrowser()
  
-CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.RunKatalon'('TC01_CDS_Filter_Study-A.xlsx')
+CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.RunKatalon'('TC03_CDS_Filter_Study-Molecular Char Init.xlsx')
+
+CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.clickTab'('Object Repository/Bento/Banner/Bento_Warning_Continue_Btn')
+System.out.println ("Closed the warning window");
 
 //Clicking data tab
 WebUI.waitForElementPresent(findTestObject('Object Repository/CDS/NavBar/CDS_Data-Btn'),5)
@@ -52,12 +55,11 @@ WebUI.waitForElementPresent(findTestObject('Object Repository/CDS/Filter/Study/S
 CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.clickTabCDSStat'('Object Repository/CDS/Filter/Study/Study_Ddn')
 
 //Clicking Study-A checkbox
-WebUI.waitForElementPresent(findTestObject('CDS/Filter/Study/CDS-Study-A-Chkbx'),5)
-CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.clickTabCDSStat'('CDS/Filter/Study/CDS-Study-A-Chkbx')
+WebUI.waitForElementPresent(findTestObject('CDS/Filter/Study/CDS-Study-HTAN-Chkbx'),5)
+CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.clickTabCDSStat'('CDS/Filter/Study/CDS-Study-HTAN-Chkbx')
 
 //Read statbar
-CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.readStatBarCDS'('Object Repository/CDS/StatBar/CDS_StatBar-Studies',
-	'Object Repository/CDS/StatBar/CDS_StatBar-DiseaseSites', 'Object Repository/CDS/StatBar/CDS_StatBar-Participants',
+CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.readStatBarCDS'('Object Repository/CDS/StatBar/CDS_StatBar-Studies', 'Object Repository/CDS/StatBar/CDS_StatBar-Participants', 
 	'Object Repository/CDS/StatBar/CDS_StatBar-Samples', 'Object Repository/CDS/StatBar/CDS_StatBar-Files')
 
 //Clicking cases tab
@@ -68,17 +70,17 @@ CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.multiFunction'('CDS',Global
 	GlobalVariable.G_CypherTabnameCases, GlobalVariable.G_QueryCasesTab)
 
 //clicking Samples tab
-//WebUI.waitForElementPresent(findTestObject('Object Repository/CDS/Cases_page/CDSResults_Samples_Tab'), 5)
-//CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.clickTab'('Object Repository/CDS/Cases_page/CDSResults_Samples_Tab')
-//CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.multiFunction'('CDS',GlobalVariable.G_StatBar_Publications, 'Object Repository/CDS/Cases_page/CDS_SamplesTable',
-//	'Object Repository/CDS/Cases_page/CDS_SamplesTableHeader', 'Object Repository/CDS/Cases_page/CDS_SamplesTabNextBtn', GlobalVariable.G_WebTabnameSamples,
-//	GlobalVariable.G_CypherTabnameSamples, GlobalVariable.G_QuerySamplesTab)
+WebUI.waitForElementPresent(findTestObject('Object Repository/CDS/Cases_page/CDSResults_Samples_Tab'), 5)
+CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.clickTab'('Object Repository/CDS/Cases_page/CDSResults_Samples_Tab')
+CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.multiFunction'('CDS',GlobalVariable.G_StatBar_Publications, 'Object Repository/CDS/Cases_page/CDS_SamplesTable',
+	'Object Repository/CDS/Cases_page/CDS_SamplesTableHeader', 'Object Repository/CDS/Cases_page/CDS_SamplesTabNextBtn', GlobalVariable.G_WebTabnameSamples,
+	GlobalVariable.G_CypherTabnameSamples, GlobalVariable.G_QuerySamplesTab)
 	
 //clicking Files tab
-WebUI.waitForElementPresent(findTestObject('Object Repository/CDS/Cases_page/CDSResults_Files_Tab'), 5)
-CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.clickTab'('Object Repository/CDS/Cases_page/CDSResults_Files_Tab')
-CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.multiFunction'('CDS',GlobalVariable.G_StatBar_ClinTrials, 'Object Repository/CDS/Cases_page/CDS_FilesTable',
-	'Object Repository/CDS/Cases_page/CDS_FilesTableHeader', 'Object Repository/CDS/Cases_page/CDS_FilesTabNextBtn', GlobalVariable.G_WebTabnameFiles,
-	GlobalVariable.G_CypherTabnameFiles, GlobalVariable.G_QueryFilesTab)
+//WebUI.waitForElementPresent(findTestObject('Object Repository/CDS/Cases_page/CDSResults_Files_Tab'), 5)
+//CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.clickTab'('Object Repository/CDS/Cases_page/CDSResults_Files_Tab')
+//CustomKeywords.'ctdc.utilities.runtestcaseforKatalon.multiFunction'('CDS',GlobalVariable.G_StatBar_ClinTrials, 'Object Repository/CDS/Cases_page/CDS_FilesTable',
+//	'Object Repository/CDS/Cases_page/CDS_FilesTableHeader', 'Object Repository/CDS/Cases_page/CDS_FilesTabNextBtn', GlobalVariable.G_WebTabnameFiles,
+//	GlobalVariable.G_CypherTabnameFiles, GlobalVariable.G_QueryFilesTab)
  
 WebUI.closeBrowser()
