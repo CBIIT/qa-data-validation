1a70e442d318fba50359dd3954e3616631425b57
Radhakrishnan <radhakrishnang2@nih.gov>
2023-03-16 15:21:01 -0700

    MTP test suite 22.11

--- Test Suites/MTP_Regression_DV3_22.11_SAv11.1_GXv10.0.ts
+++ Test Suites/MTP_Regression_DV3_22.11_SAv11.1_GXv10.0.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>MTP_Regression</name>
+   <name>MTP_Regression_DV3_22.11_SAv11.1_GXv10.0</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
