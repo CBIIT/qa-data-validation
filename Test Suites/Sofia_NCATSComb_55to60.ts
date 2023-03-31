ead4a85486612a06e07c96ff038e234da41d92aa
Sofia David <davids3@nih.gov>
2023-03-31 11:36:36 -0400

    NCATS Study TC45,5155TO60

---
+++ Test Suites/Sofia_NCATSComb_55to60.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sofia_NCATSComb_55to60</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7b141eff-cedf-46de-a1fa-3058df458eae</testSuiteGuid>
+   <testCaseLink>
+      <guid>db29e18f-569c-4dd2-9600-618574d1b316</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-NCATSCOP01/TC55_Canine_StudyNCATSCOP01-Sex_FileAsso_FileType_FileFormat</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1a1971ea-2325-4bca-863a-8333dc440926</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-NCATSCOP01/TC56_Canine_StudyNCATSCOP01-Breed_SampleSite_SampleType</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>04a30b9f-4e31-4097-a59b-71b90c14988e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-NCATSCOP01/TC57_Canine_StudyNCATSCOP01-Diagnosis_SamplePath_FileAsso_FileType</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>55a4987a-7150-4615-ad1b-1f75cec50b31</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-NCATSCOP01/TC58_Canine_StudyNCATSCOP01-StageOfDisease_SampleSite_FileType</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6bd75be8-2de2-4f08-bbf1-93c7bb63e0ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-NCATSCOP01/TC59_Canine_StudyNCATSCOP01-StudyPart_Multi_FileType_FileFormat</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d51cf6b8-93fc-42ca-ad44-66faa6ac64bd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-NCATSCOP01/TC60_Canine_StudyNCATSCOP01-SampleSite_FileAsso_FileType</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
