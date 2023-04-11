a175dda1fd36f826aabe04c0ce194582f66092f3
ValentinaEp <valentina.epishina@nih.gov>
2023-04-11 14:58:44 -0400

    ICDC_COTC022 test suit

--- Test Suites/ICDC_COTC022.ts
+++ Test Suites/ICDC_COTC022.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>ICDC_Test</name>
+   <name>ICDC_COTC022</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,170 +10,170 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>da95ef46-5b1b-476f-ab7f-bbecad9cf1e7</testSuiteGuid>
+   <testSuiteGuid>d9e16f8d-98d5-49fa-9af4-ba6aa90970a4</testSuiteGuid>
    <testCaseLink>
-      <guid>2f735fad-71a2-4c94-89b5-bd5673cd51d1</guid>
+      <guid>7694ab93-3435-4dde-b8c8-2521b4ebb7a5</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC01_Canine_StudyCOTC022-StudyType_Breed_Sex</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>78dcd967-ae65-4b65-a8ec-2663db7b2a14</guid>
+      <guid>454effcf-172a-458b-8a97-fd43350f6ed1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC02_Canine_StudyCOTC022-StudyType_Breed_ResponseToTreatment_NeuteredStatus</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>86125c7f-aad8-4d12-81b4-4d8a545d5047</guid>
+      <guid>290620aa-38ce-4627-8070-a72f08e35b0e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC03_Canine_StudyCOTC022-StudyType_PrimaryDiseaseSite_ResponseToTreatment_Sex</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>da8ef00a-a767-413d-901e-df682174c0d4</guid>
+      <guid>a45a5d7c-1cff-45be-90aa-d1f82362cdfa</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC04_Canine_StudyCOTC022-StudyType_StudyParticipation_Diagnosis_NeutStatus</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>fdcc9197-168a-43ea-8c9b-d98b5f2750ae</guid>
+      <guid>3c31c28d-85a5-48a5-b4c8-7a878a42152c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC05_Canine_StudyCOTC022-StudyParticipation_Breed_NeuteredStatus</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>1316d03a-2a27-45ba-8c96-bcbc07dd4a4a</guid>
+      <guid>c4a77d87-8b0b-435c-8bd8-ecc3e0288d08</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC06_Canine_StudyCOTC022-StudyParticipation_Breed_PrimaryDiseaseSite_ResponseToTreatment</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>19c87019-1d8e-4647-b404-2d7789cebb8f</guid>
+      <guid>36ceab3a-4fe6-4723-98f8-9d54a0a611d7</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC07_Canine_StudyCOTC022-StudyParticipation_PrimaryDiseaseSite_ResponseToTreatment</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>d9e4057d-7b3a-44d9-8687-3454c616a6e4</guid>
+      <guid>a97fefb2-0b67-4679-933b-f4d7b25466f2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC08_Canine_StudyCOTC022-StudyParticipation_Diagnosis_PrimaryDiseaseSite</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>cc11d171-fca6-4070-b136-9bd17775fbfc</guid>
+      <guid>844d3c92-60ec-4112-8070-fa4fd4e08b30</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC09_Canine_StudyCOTC022-StudyParticip_StageOfDisease_ResponseToTreat_Sex</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>faac6e19-83d5-42c5-9f9e-b2fd79a483a2</guid>
+      <guid>075ae4a5-11a2-4c0c-8c2c-564175b7223a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC10_Canine_StudyCOTC022-Breed_Diagnosis_ResponseToTreat</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>dce31713-9f25-4bf5-a408-8a74ecd05810</guid>
+      <guid>1dcf482f-335d-4b22-8dd8-46e1907f4111</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC11_Canine_StudyCOTC022-Breed_PrimDiseaseSite_ResponseToTreat</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>edd74e96-809a-4682-bd5f-5c109fdd5e50</guid>
+      <guid>8cbc7be4-18f8-45ec-bcbc-853a0ad01a9a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC12_Canine_StudyCOTC022-Breed_PrimDiseaseSite_Sex</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>c0fa04b5-bdbf-43f9-b72e-12ec13f496d2</guid>
+      <guid>5c74c89f-0969-4ddd-9f83-5c27a3765ede</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC13_Canine_StudyCOTC022-Breed_ResponsToTreatmet_Sex</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>51fc4ae3-0e57-47cd-b8aa-89ad7c1cb476</guid>
+      <guid>b9ae002d-2593-467f-a965-97be9e87567b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC14_Canine_StudyCOTC022-Breed_Sex_NeuteredStatus</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bc9e7460-0c22-48ae-8159-8ea19a7a0c19</guid>
+      <guid>112a3977-551b-46a3-9d3d-320e66747c5d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC15_Canine_StudyCOTC022-Diagnosis_StageOfDisease_Sex_NeuterStats</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bee93eed-ed42-4118-be56-9376bdd23d9d</guid>
+      <guid>5d9a0a6c-5c77-4b82-8719-b341c4455062</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC16_Canine_StudyCOTC022-PrimDiseaseSite_RespToTreatnt_Sex_NeutStats</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>55f8e688-28ff-4aa0-b4a0-01bdd5510573</guid>
+      <guid>a21e90cb-ea33-4645-85b8-6b8aa8642fdd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC17_Canine_StudyCOTC022-StudyType_Bioblank_NeuterStats</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>82774b43-4259-4fc1-975a-482e9ed7f5c7</guid>
+      <guid>940ba1ba-f44a-4354-9776-01d38f85e6c4</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC18_Canine_StudyCOTC022-Bioblank_Breed_Sex</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>7cbed25c-519d-4339-b91b-a3aad8f97151</guid>
+      <guid>53d9416c-b20c-4aca-85a7-f144ffd74926</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC19_Canine_StudyCOTC022-Bioblank_Breed_ResponsToTretmnt</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>8497b808-a2b0-43b8-b815-727345664cc6</guid>
+      <guid>39643ef3-9514-4d8f-a066-f8346af1c1ea</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC20_Canine_StudyCOTC022-Bioblank_Diagnosis_PrimDiseaseSite_Sex</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>91006548-b34a-44b2-b5cb-7fc1c1ff5d5d</guid>
+      <guid>86168bba-a643-4759-8a46-8c789918b9ec</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC21_Canine_StudyCOTC022-Bioblank_PrimDiseaseSite_RespToTereatmnt</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>cd24095a-9054-4076-a37a-8fbf26f3485f</guid>
+      <guid>bb27bfe6-ef06-4bd5-998e-295bc07a6cbd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC22_Canine_StudyCOTC022-Bioblank_StageOfDisease_Sex_NeuterStats</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>e4ab9bcc-8c89-4e75-b0e5-a3e04c319e4a</guid>
+      <guid>619393df-9393-4f46-82d3-55240fa236ca</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC23_Canine_StudyCOTC022-StageOfDisease_Sex_NeuterStats</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>bbcdaca0-5c9c-4c04-b79c-229a03236eeb</guid>
+      <guid>3ff41d56-4bd8-49b7-a198-973dc3f6b5a2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Canine_TestCases/StudyCombinations/Study-Combinations-COTC022/TC24_Canine_StudyCOTC022-StageOfDisease_RespnseToTreat_NeuterStats</testCaseId>
