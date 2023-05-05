67df96fcee16953b7df27203a59473d4f2a70a11
Sofia David <davids3@nih.gov>
2023-05-05 16:05:56 -0400

    ICDC Response to treatment filter -Fixed existing test cases

---
+++ Test Suites/Sofia_ResptoTrtmnt.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sofia_ResptoTrtmnt</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>26957ab6-1fd5-49e1-a216-027701748d82</testSuiteGuid>
+   <testCaseLink>
+      <guid>02a620c9-0d51-4c88-94b2-84ae7f06ec5e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/ResponseToTreatment/TC02_Canine_Filter_RespToTrtmt-NotApplicable</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d2467a40-9b2d-488a-a1ae-abe474a110af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/ResponseToTreatment/TC01_Canine_Filter_RespToTrtmt-CompleteResponse</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0dc53111-35c0-4815-9aa5-283e9ce833d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Canine_TestCases/ResponseToTreatment/TC03_Canine_Filter_RespToTrtmt-NotDetermined</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
