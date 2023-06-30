97b5df3e44470f01d656ca50f90daca928102001
radhakrishnang2 <radhakrishnang2@host.docker.internal>
2023-06-30 09:38:08 -0700

    crai cram added

---
+++ Object Repository/CDS/Data_page/Filter/FileType/crai-Chkbx.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>crai-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>8ead1331-f311-440f-9594-08c69b31cb5b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_File Type_CRAI&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_File Type_CRAI&quot;]</value>
+      <webElementGuid>82ffaf13-d278-4a6a-afbe-d0e1ff13c7a6</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/CDS/Data_page/Filter/FileType/cram-Chkbx.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>cram-Chkbx</name>
+   <tag></tag>
+   <elementGuidId>1002db90-b861-4cfd-bd09-0cc17705e091</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_File Type_CRAM&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_File Type_CRAM&quot;]</value>
+      <webElementGuid>6af52c25-3ec0-457c-890c-3731a893b4a8</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
