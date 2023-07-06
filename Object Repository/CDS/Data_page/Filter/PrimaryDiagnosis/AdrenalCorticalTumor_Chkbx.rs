1157bd85d9f306ff648434e16d6ff87a2f0d5283
Sofia David <davids3@nih.gov>
2023-07-06 15:14:27 -0400

    CDS primary Diagnosis filter

---
+++ Object Repository/CDS/Data_page/Filter/PrimaryDiagnosis/AdrenalCorticalTumor_Chkbx.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>AdrenalCorticalTumor_Chkbx</name>
+   <tag></tag>
+   <elementGuidId>db1d0237-4acb-401a-8725-fd449b329a56</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;checkbox_PRIMARY DIAGNOSIS_Adrenal Cortical Tumor&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;checkbox_PRIMARY DIAGNOSIS_Adrenal Cortical Tumor&quot;]</value>
+      <webElementGuid>964f529c-1d98-43e3-bf2e-85744db260a5</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
