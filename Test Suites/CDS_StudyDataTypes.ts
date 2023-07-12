80cf8e65a7ad88e072667038f2594c1afb64889d
ValentinaEp <valentina.epishina@nih.gov>
2023-07-12 15:06:49 -0400

    CDS_StudyDataTypes test Suite

---
+++ Test Suites/CDS_StudyDataTypes.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CDS_StudyDataTypes</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>990e4e5c-b870-403a-b219-35360e733067</testSuiteGuid>
+   <testCaseLink>
+      <guid>49f1e644-c3bb-4a82-a6ae-0ba0ed7bcc44</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/StudyDataTypes/TC01_CDS_Filter_StudyDataTypes-Genomic</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
