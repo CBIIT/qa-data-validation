88ae9ed51032f5cb8746d090c7e032c15af56588
Lpodagat <lakshmip881@gmail.com>
2023-07-26 12:53:45 -0400

    cds test suites

---
+++ Test Suites/CDS LibararySelection.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CDS LibararySelection</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e3c7f762-2ea9-4905-a083-5e68313001f2</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb20a069-db7a-4594-8c29-5291e97090ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC01_CDS_Filter_LibrarySelection-HybridSelection</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db3cd489-df47-4a9c-8cef-f2e543ddcae1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC02_CDS_Filter_LibrarySelection-NotSpecifiedinData</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d7576c7e-850b-43b0-8259-4a97f6e7d790</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC03_CDS_Filter_LibrarySelection-PCR</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e026726a-e5a5-405d-873c-b299f97e5f0d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC04_CDS_Filter_LibrarySelection-PolyA</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>82e9f3ca-5a17-4ff8-8607-e46cf0d2d518</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC05_CDS_Filter_LibrarySelection-Poly-TEnrichment</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>244b3b69-833b-43e6-a21c-ddf3e7790ae9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC06_CDS_Filter_LibrarySelection-RANDOM</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d84620c7-e3dd-458e-aef1-3a985dbe91e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC07_CDS_Filter_LibrarySelection-rRNADepletion</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>48e8e0db-ddfe-4941-b4f0-792beb173b08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC08_CDS_Filter_LibrarySelection-other</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cedda6ca-6a15-4c17-a696-9ac86c9516be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CDS_TestCases/LibrarySelection/TC09_CDS_Filter_LibrarySelection_random</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
