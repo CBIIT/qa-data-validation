223bfe315cd5c2702bf0890346ceac5fc00eb21a
Lpodagat <lakshmip881@gmail.com>
2023-07-26 12:57:37 -0400

    cds test suites

--- Test Suites/CDS LibraryLayout.ts
+++ Test Suites/CDS LibraryLayout.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>LibraryLayout</name>
+   <name>CDS LibraryLayout</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
